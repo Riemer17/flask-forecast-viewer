f39cd2e7c85138f857c929d72c22752c740bd751
Andre Roukema <andre.roukema@live.nl>
2020-07-08 19:28:04 +0200

    Update visualization code: png generated - generate png of chart of day data in the zip

--- buienradar_forecast_visualization.py
+++ buienradar_forecast_visualization.py
@@ -9,6 +9,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from pandas.tseries.offsets import BDay
+import matplotlib.colors as mcolors
+
 
 pd.set_option('display.max_colwidth', -1)
 
@@ -49,6 +51,7 @@ class ForecastVizualization():
 
     def __init__(self):
         self.zip_file = fr'results/forecasts.zip'
+        # self.zip_file = fr'c:\data\forecasts.zip'
 
     def store_item(self, k, v, day):
         self.days[k][day.get('datetime')].append([self.ts, v])
@@ -62,8 +65,71 @@ class ForecastVizualization():
             v = [[self.store_item(k, v, day) for k, v in day.items() if not isinstance(v, (dict, list))]
                  for day in data['days']]
 
-        print(self.days)
+    def handle_sub_key(self, kk, v, k):
+        if isinstance(v, list) and not k == 'datetime':
+            df_w = pd.DataFrame(v, columns=['ts', k])
+            for row in df_w.iterrows():
+                self.df_hm.loc[kk, row[1][0]] = row[1][1]
+        else:
+            return v
 
+    def handle_key(self, k):
+        self.category = k
+        self.df_hm = pd.DataFrame()
+        self.chart_data = [(f'{kk}: {self.handle_sub_key(kk, v, k)} ') for kk, v in self.days[k].items()]
+
+    def get_rvb(self, category):
+        clist = {'temperature': [(0, "blue"), (.2, "lightblue"), (.35, "green"), (.55, "yellow"), (.7,                      "orange"),(.8, "red"), (1, "purple")],
+                 'maxtemperature': [(0, "blue"), (.2, "lightblue"), (.35, "green"), (.55, "yellow"), (.7,                   "orange"), (.8, "red"), (1, "purple")],
+                 'mintemperature': [(0, "blue"), (.2, "lightblue"), (.35, "green"), (.55, "yellow"), (.7,                   "orange"), (.8, "red"), (1, "purple")],
+                 'humidity': [(0, "lightblue"), (.2, "blue"), (.35, "green"), (.55, "yellow"), (.7,                         "orange"), (.8, "red"), (1, "purple")],
+                 'windspeedms': [(0, "lightblue"), (.2, "blue"), (.35, "green"), (.55, "yellow"), (.7,                      "orange"), (.8, "red"), (1, "purple")],
+                 'sunshine': [(0, "white"), (.50, "grey"), (1, "yellow")],
+                 'pollenindex': [(0, "green"), (.25, "lightblue"), (.35, "blue"), (.5, "yellow"), (.7,                      "orange"), (.8, "red"), (1, "purple")],
+                 'precipitationmm': [(0, "white"), (.001, "white"), (.01, "lightblue"), (.2, "blue"), (.35,                 "green"), (.55, "yellow"), (.7, "orange"), (.8, "red"), (1, "purple")],
+                 'precipitation': [(0, "white"), (.01, "white"), (.2, "lightblue"), (.35, "green"), (.55,                   "yellow"), (.7, "orange"), (.8, "red"), (1, "purple")],
+                 'beaufort': [(0, "blue"), (.2, "lightblue"), (.35, "green"), (.55, "yellow"), (.7,                         "orange"), (.8, "red"), (1, "purple")]}
+        return mcolors.LinearSegmentedColormap.from_list("", clist[category])
+
+    def min_max(self, category):
+        return {'temperature': (-10, 40), 'humidity': (0, 100), 'beaufort': (0, 12),
+                'precipitation': (0, 10), 'precipitationmm': (0, 100), 'sunshine': (0, 100),
+                'pollenindex': (0, 10), 'maxtemperature': (-10, 40),
+                'mintemperature': (-10, 40), 'windspeedms': (0, 100)}[category]
+
+    def set_annotations(self, ax, df):
+        for i in range(len(df)):
+            for j in range(len(df.columns)):
+                if not str(df.iloc[i, j]) == str(np.nan):
+                    text = ax.text(j, i, df.iloc[i, j], ha="center", va="center", color="black")
+
+    def set_ticks(self, ax, df):
+        # We want to show all ticks...
+        ax.set_xticks(np.arange(len(df.columns)))
+        ax.set_yticks(np.arange(len(df)))
+        # ... and label them with the respective list entries
+        ax.set_xticklabels(df.columns)
+        ax.set_yticklabels(df.index)
+        #     ax.xaxis.set_ticks_position("top")
+        plt.setp(ax.get_xticklabels(), rotation=45, ha="right", rotation_mode="anchor")
+
+    def visualize_category(self, category):
+        self.handle_key(category)
+        first_day = (dt.today() - BDay(2)).strftime('%Y-%m-%dT%H:%M:%S')
+        df_hm2 = self.df_hm[self.df_hm.index >= first_day]
+        df_hm2 = df_hm2.dropna(how='all', axis=1).sort_index(axis=0).sort_index(axis=1, ascending=False)
+        vmin, vmax = self.min_max(category)
+        fig, ax = plt.subplots(figsize=(75, 75))
+        im = ax.imshow(df_hm2, cmap=self.get_rvb(category), interpolation='nearest', vmin=vmin, vmax=vmax)
+        self.set_annotations(ax, df_hm2)
+        self.set_ticks(ax, df_hm2)
+        plt.grid(alpha=.4)
+        fig.tight_layout()
+        fig.savefig(rf'results/bu_day_{category}.png')
+        # fig.savefig(rf'c:\data\bu_day_{category}.png')
 
 fv = ForecastVizualization()
 fv.get_days()
+fv.visualize_category('mintemperature')
+fv.visualize_category('maxtemperature')
+fv.visualize_category('precipitationmm')
