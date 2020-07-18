615ad4b73de4aa7a108f17d5a4fb734486fee7b0
Andre Roukema <andre.roukema@live.nl>
2020-07-18 19:27:55 +0200

    dynamic content fomr select with dummy content

--- flask-forecast-viewer.py
+++ flask-forecast-viewer.py
@@ -1,6 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
+def create_select(options, selected=0):
+    action = 'window.location.href="this.value"'
+    options = '\n'.join([f'<option value={v}>v</>' for v in options])
+    return f'<select name="colour" method="GET" action="/" onchange=\'{action}\'>{options}</select>'
+
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    chart_select = create_select(range(20))
+    return render_template('index.html', chart_select=chart_select)
+
+@app.route('/<field>')
+def pick_chart(field):
+    return f'Soon here you will see {field}.png.'
+
