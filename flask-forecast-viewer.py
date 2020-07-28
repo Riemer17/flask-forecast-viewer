278c0d4845408a7b7f3df8d4676125e2504c9f79
Andre Roukema <andre.roukema@live.nl>
2020-07-28 22:14:52 +0200

    created resources folder, dynamic content for charts, css changes, new endpoint with parameter

--- flask-forecast-viewer.py
+++ flask-forecast-viewer.py
@@ -1,11 +1,27 @@
+import zipfile
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 def create_select(options, selected=0):
-    action = 'window.location.href="this.value"'
-    options = '\n'.join([f'<option value={v}>v</>' for v in options])
+    action = 'window.location.href=this.value'
+    options = '\n'.join([f'<option value={v}>{v}</option>' for v in options])
     return f'<select name="colour" method="GET" action="/" onchange=\'{action}\'>{options}</select>'
+
+
+def create_img(field):
+    return field
+
+
+def tag(the_tag, txt, attributes=''):
+    return '<{tag}{attributes}>{txt}</{tag}>'.format(tag=the_tag, txt=txt,
+           attributes=(('' if attributes == '' else ' ') + attributes))
+
+def read_zip():
+    zip_name = r"resources/forecasts.zip"
+    forecasts = zipfile.ZipFile(zip_name, 'r')
+    return ''.join([tag('tr',tag('td', file)) for file in forecasts.namelist()])
 
 
 @app.route('/')
@@ -15,5 +31,9 @@ def index():
 
 @app.route('/<field>')
 def pick_chart(field):
-    return f'Soon here you will see {field}.png.'
+    chart_select = create_select(range(20))
+    img_source = create_img(field)
+    zip_files = read_zip()
+    return render_template('chart.html', chart_select=chart_select,
+                           img_source=img_source, zip_files=zip_files)
 
