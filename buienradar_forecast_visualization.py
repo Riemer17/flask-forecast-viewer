9465b69399382dac6693ccc4191de23309e44f9c
Andre Roukema <andre.roukema@live.nl>
2020-08-01 12:03:38 +0200

    change size of the charts generated by the scheduled task

--- buienradar_forecast_visualization.py
+++ buienradar_forecast_visualization.py
@@ -120,7 +120,7 @@ class ForecastVizualization():
         df_hm2 = self.df_hm[self.df_hm.index >= first_day]
         df_hm2 = df_hm2.dropna(how='all', axis=1).sort_index(axis=0).sort_index(axis=1, ascending=False)
         vmin, vmax = self.min_max(category)
-        fig, ax = plt.subplots(figsize=(30, 30))
+        fig, ax = plt.subplots(figsize=(30, 12))
         im = ax.imshow(df_hm2, cmap=self.get_rvb(category), interpolation='nearest', vmin=vmin, vmax=vmax)
         self.set_annotations(ax, df_hm2)
         self.set_ticks(ax, df_hm2)
